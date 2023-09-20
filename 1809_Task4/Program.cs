ec5263fa1d53095c9c90e46db5c295e1a74e2bf0
AnastasiiaAleks <chiffka@gmail.com>
2023-09-21 00:25:05 +0300

    task 4

---
+++ 1809_Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Напишите сюда первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Теперь еще одно число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+Console.Write("Я хочу еще одно число: ");
+int number3 = int.Parse(Console.ReadLine());
+
+int max = number1;
+
+if (number1 > max) max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+
+Console.Write("Найдено максимальное число. ЭТО: " + max + "!");
