c892134c84ee4fe933999b9a5123af1733f6a55c
AnastasiiaAleks <chiffka@gmail.com>
2023-09-21 00:38:13 +0300

    task6

---
+++ 1809_Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Напиши мне число: ");
+int number = int.Parse(Console.ReadLine());
+
+int remainder = number % 2;
+
+if (remainder == 0)
+{
+    Console.Write("Молодец, " + number + " - это четное число.");
+}
+else
+{
+    Console.Write(number + " - это нечетное число, пополам без остатка не получится.");
+}
