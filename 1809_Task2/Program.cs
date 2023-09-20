0fbf15159e44c7e901446158b6c0b4655aba7ca7
AnastasiiaAleks <chiffka@gmail.com>
2023-09-20 22:12:47 +0300

    Task 2

---
+++ 1809_Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine("Максимальное число: " + number1 + ", минимальное число: " + number2);
+}
+else if (number1 == number2)
+{
+    Console.WriteLine("Поздравляем, эти числа равны, попробуйте еще раз!");
+}
+else
+{
+    Console.WriteLine("Максимальное числов: " + number2 + ", минимальное число: " + number1);
+}
